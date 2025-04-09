24c4e0bfbd900ad002e467afa0b936ec435e7a63
lsc11232 <1728922396@qq.com>
2025-04-09 08:38:22 +0000

    fix:update env

--- .ci/install.sh
+++ .ci/install.sh
@@ -17,7 +17,8 @@ source .env/bin/activate
 python3 -m pip install -U pip
 
 # torch cuda 11.3
-python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu113
+python3 -m pip install "torch==2.1.2" torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+python3 -m pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.1.2+cu121.html
 
 # dgl cuda 11.3
 python -m pip install dgl==1.1 -f https://data.dgl.ai/wheels/cu113/repo.html
